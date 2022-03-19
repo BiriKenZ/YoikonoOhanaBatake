ea29f16bf51a4b953b681b5846438cd29dc5b3ad
MasaoSasaki <65378642+MasaoSasaki@users.noreply.github.com>
2022-03-20 08:59:36 +0900

    ✨ Todo削除機能の非同期処理を実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,15 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_todo, only: [:index, :create, :update, :destroy]
+
   def index
     @todo = Todo.new
-    @todos = Todo.all
   end
 
   def update
   end
 
   def create
-    @todos = Todo.all
     todo = Todo.new(todo_params)
     todo.user_id = current_user.id
     todo.flower_id = rand(Flower.count)
@@ -18,15 +18,17 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    todo = Todo.find(params[:id])
-    todo.destroy
-    redirect_to '/todos'
+    Todo.find(params[:id]).destroy
   end
 
   private
   # ストロングパラメータ
   def todo_params
     params.require(:todo).permit(:title, :comment, :is_completed)
+  end
+
+  def set_todo
+    @todos = Todo.all
   end
 
 end
