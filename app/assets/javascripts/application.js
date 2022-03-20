a69b71d0d37d1d4130af70cb5535e20ab3497aec
nishimurasyota <syota4723@gmail.com>
2022-03-20 11:32:08 +0900

    Todo一覧の高さ調整の関数を実装、デザインを変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,20 @@ $(function () {
     $(".modalsearch,.masksearch").fadeOut();
   });
 });
+
+// 高さを整える関数
+$(function () {
+  // 高さの最大値を代入するための変数h
+  var h = 0;
+
+  // それぞれの要素の高さを調べる
+  $("li.todo").each(function () {
+    // 要素の高さと変数hの値を比較し、大きい方を変数hに代入
+    if ($(this).height() > h) {
+      h = $(this).height() + 40;
+    }
+  });
+
+  // 要素の高さを変数hの値に統一します
+  $("li.todo").css("height", h + "px");
+});
